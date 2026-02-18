95d6794aeec0eb9780ab64921e8f5e903e54c586
samueldavidpolo112006-afk <samueldavidpolo112006@gmail.com>
2026-02-18 23:54:14 +0000

    EV04 - Taller Control Condicional CSharp

---
+++ TallerControlCondicional/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== TALLER CONTROL CONDICIONAL ===");
+
+        Console.Write("Ingrese su edad: ");
+        int edad = int.Parse(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.WriteLine("Eres mayor de edad.");
+        }
+        else
+        {
+            Console.WriteLine("Eres menor de edad.");
+        }
+
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("El número es positivo.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("El número es negativo.");
+        }
+        else
+        {
+            Console.WriteLine("El número es cero.");
+        }
+
+        Console.WriteLine("Programa finalizado.");
+    }
+}
